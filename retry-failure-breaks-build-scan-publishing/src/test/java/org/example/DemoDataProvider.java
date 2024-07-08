199376f835807daeae8f47f703e6c91f9590dc9d
Payne Chen <payne.chen@sap.com>
2024-07-08 14:00:18 +0800

    setup code example to reproduce build scan publishing proboem (#1)
    * setup sample code to reproduce build scan publishing failure
    
    * change test name

---
+++ retry-failure-breaks-build-scan-publishing/src/test/java/org/example/DemoDataProvider.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import org.testng.annotations.DataProvider;
+
+public class DemoDataProvider {
+    @DataProvider
+    public Object[][] testDataProvider() {
+        return new Object[][]{
+                {1},
+                {2},
+                {3},
+                {4},
+                {5},
+                {6},
+                {7},
+                {8},
+                {9},
+                {10},
+                {11},
+                {12},
+                {13},
+                {14},
+                {15},
+                {16},
+                {17},
+                {18},
+                {19},
+                {20},
+                {21},
+                {22},
+                {23},
+                {24}
+        };
+    }
+}
